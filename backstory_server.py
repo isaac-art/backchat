f33a2bd1eebbf4c7dbd48038d096a50daad87380
Isaac <isaac@isaacclarke.com>
2025-07-06 18:01:09 +0800

    local

--- backstory_server.py
+++ backstory_server.py
@@ -154,4 +154,4 @@ async def shutdown_event():
 ###     BEGIN     ###
 #####################
 if __name__ == "__main__":
-    uvicorn.run(app, host="0.0.0.0", port=8081)
+    uvicorn.run(app, host="localhost", port=8081)
