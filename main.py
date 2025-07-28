ed6bf9f5f7e9009f5c528fd376caf4d09b607f88
isaac <isaac@ishimura.uk>
2025-07-28 11:19:03 +0100

    Update main.py

--- main.py
+++ main.py
@@ -190,4 +190,4 @@ async def shutdown_event():
 ###     BEGIN     ###
 #####################
 if __name__ == "__main__":
-    uvicorn.run(app, host="localhost", port=5080)
+    uvicorn.run(app, host="127.0.0.1", port=5080)
